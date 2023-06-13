144323f8fd8e2a56fc32df626cfa2d0b14b1fc87
Ricardo Camargo Favaro <ri_ca_fa@hotmail.com>
2023-06-12 22:47:44 -0300

    add cliente crud tests

---
+++ tests/Feature/Integration/ClientesCrudTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Integration;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class Clientes extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_clientes_index_ok()
+    {
+        $response = $this->get('/clientes');
+        $response->assertStatus(200);
+    }
+    public function test_clientes_create_ok()
+    {
+        $response = $this->get('/clientes/create');
+        $response->assertStatus(200);
+    }
+    public function test_clientes_update_ok()
+    {
+        $response = $this->get('/clientes/1');
+        $response->assertStatus(200);
+    }
+}
